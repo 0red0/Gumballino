be84c2ad82de5c20cbbf603d2b61ecd710ef20bc
yahya <yhyawy1@gmail.com>
2022-12-08 14:44:49 +0200

    initalize webpack first time

---
+++ src/gumMain.js
@@ -0,0 +1,2 @@
+import { message } from "./gumIce.js";
+alert(message);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: "./src/gumMain.js",
+  output: {
+    filename: "gumballino.js",
+    path: path.resolve("dist"),
+  },
+  mode: "development",
+};
