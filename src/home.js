9279dc8530d12dfd55da959e234dc5b92b2044df
yhyawy <yhyawy1@gmail.com>
2022-12-11 21:03:16 +0200

    home page done

--- src/home.js
+++ src/home.js
@@ -1,3 +1,18 @@
+import overlay from "./confetti.js";
 export default (function () {
-   console.log("home");
+   window.addEventListener("load", party);
+   function party() {
+      const homeOverlay = document.querySelector(".home .home-overlay");
+      let horn = document.getElementById("horn");
+      horn.play();
+      homeOverlay.addEventListener("click", () => {
+         horn.play();
+      });
+      setTimeout(() => {
+         homeOverlay.classList.add("hide");
+         setTimeout(() => {
+            homeOverlay.classList.add("none");
+         }, 500);
+      }, 4000);
+   }
 })();
