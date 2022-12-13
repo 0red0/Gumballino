67a9c81c99ea215af9e1ef132d3aeff2b4e03f48
yhyawy <yhyawy1@gmail.com>
2022-12-13 22:22:59 +0200

    menu page done

--- src/home.js
+++ src/home.js
@@ -4,9 +4,12 @@ export default (function () {
 })();
 
 const homeOverlay = document.querySelector("#home .home-overlay");
-const menuBtn = document.querySelector(".menu-holder");
+const menuBtn = document.querySelectorAll(".menu-holder");
+const homeBtn = document.querySelectorAll(".goto-home");
+const aboutBtn = document.querySelectorAll(".burger-holder");
 const homePage = document.getElementById("home");
 const menuPage = document.getElementById("menu");
+const aboutPage = document.getElementById("about");
 
 function party() {
    let horn = document.getElementById("horn");
@@ -20,11 +23,26 @@ function party() {
       setTimeout(() => {
          homeOverlay.classList.add("none");
       }, 500);
-   }, 4000);
+   }, 2000);
 }
 
-menuBtn.addEventListener("click", goToMenu);
+menuBtn.forEach((b) => b.addEventListener("click", goToMenu));
 function goToMenu() {
    homePage.style.display = "none";
    menuPage.style.display = "block";
+   aboutPage.style.display = "none";
+}
+
+homeBtn.forEach((b) => b.addEventListener("click", goToHome));
+function goToHome() {
+   homePage.style.display = "block";
+   menuPage.style.display = "none";
+   aboutPage.style.display = "none";
+}
+
+aboutBtn.forEach((b) => b.addEventListener("click", goToAbout));
+function goToAbout() {
+   homePage.style.display = "none";
+   menuPage.style.display = "none";
+   aboutPage.style.display = "block";
 }
