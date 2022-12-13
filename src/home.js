fc4e0cc2b09aa1b3a2a8524c4885c21423a82f3d
yhyawy <yhyawy1@gmail.com>
2022-12-13 12:56:56 +0200

    menu slider done

--- src/home.js
+++ src/home.js
@@ -1,18 +1,30 @@
 import overlay from "./confetti.js";
 export default (function () {
    window.addEventListener("load", party);
-   function party() {
-      const homeOverlay = document.querySelector(".home .home-overlay");
-      let horn = document.getElementById("horn");
-      horn.play();
-      homeOverlay.addEventListener("click", () => {
-         horn.play();
-      });
-      setTimeout(() => {
-         homeOverlay.classList.add("hide");
-         setTimeout(() => {
-            homeOverlay.classList.add("none");
-         }, 500);
-      }, 4000);
-   }
 })();
+
+const homeOverlay = document.querySelector("#home .home-overlay");
+const menuBtn = document.querySelector(".menu-holder");
+const homePage = document.getElementById("home");
+const menuPage = document.getElementById("menu");
+
+function party() {
+   let horn = document.getElementById("horn");
+   horn.play();
+
+   homeOverlay.addEventListener("click", () => {
+      horn.play();
+   });
+   setTimeout(() => {
+      homeOverlay.classList.add("hide");
+      setTimeout(() => {
+         homeOverlay.classList.add("none");
+      }, 500);
+   }, 4000);
+}
+
+menuBtn.addEventListener("click", goToMenu);
+function goToMenu() {
+   homePage.style.display = "none";
+   menuPage.style.display = "block";
+}
